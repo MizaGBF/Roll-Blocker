f36a408bd041d2f8d8ad6532e1b1a7c4ecce9376
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2022-01-23 16:41:07 +0100

    1.0

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(){ return {cancel: true}; },
+        {
+            urls: ["http://game.granbluefantasy.jp/gacha/legend/*"],
+            types: ["xmlhttprequest"]
+        },
+    ["blocking"]
+);
---
+++ readme.md
@@ -0,0 +1,6 @@
+### GBF Roll Blocker  
+You can't roll the gacha anymore.  
+It blocks the request, nothing else.  
+  
+# Usage  
+Extension menu > enable Dev mode > load unpacked > select the folder containing those files
