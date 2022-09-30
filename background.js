0366cf8b0ccbd8021850e44551b0f522955e94b0
MizaGBF <backupgbf1@gmail.com>
2022-09-30 11:29:33 +0200

    1.2 Manifest V3

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(){ return {cancel: true}; },
-        {
-            urls: ["http://game.granbluefantasy.jp/gacha/legend/*", "https://game.granbluefantasy.jp/gacha/legend/*"],
-            types: ["xmlhttprequest"]
-        },
-    ["blocking"]
-);
